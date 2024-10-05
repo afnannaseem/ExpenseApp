ed6b32c8fda9e27f2a6111cd42b3bc355149fede
afnannaseem <139938335+afnannaseem@users.noreply.github.com>
2024-10-06 03:50:20 +0500

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,8 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "https://expenseapp-2koq.onrender.com/api";
-
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 // Lists
 export const getLists = async () => {
   return await axios.get(`${API_URL}/lists`);
