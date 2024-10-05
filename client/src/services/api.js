0135934d12c670948c91279e689f381022696ed5
afnannaseem <139938335+afnannaseem@users.noreply.github.com>
2024-10-06 02:37:01 +0500

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 // Lists
 export const getLists = async () => {
